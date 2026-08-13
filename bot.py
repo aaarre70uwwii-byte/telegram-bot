c38e24658389e4da2feb70aa244b9478057d53cc
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-13 23:05:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,19 @@
- import os
+import os
 from pyrogram import Client, filters
-from pyrogram.types import Message
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH") 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-app = Client(
-    "tia_bot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
+app = Client("tia_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 @app.on_message(filters.command("start") & filters.private)
 async def start_cmd(client, message):
-    await message.reply("✅ البوت شغال الحمدلله\nجرب /الاوامر")
+    await message.reply("✅ البوت اشتغل اخيرا")
 
 @app.on_message(filters.command("الاوامر"))
 async def help_cmd(client, message):
-    await message.reply("**الاوامر:**\n/ban - حظر\n/mute - كتم\n/id - معرفك")
+    await message.reply("الاوامر: /ban /mute /id")
 
-print("==============================")
-print("Database Connected ✅")
 print("Tia Bot Started ✅")
-print("==============================")
-
 app.run()
