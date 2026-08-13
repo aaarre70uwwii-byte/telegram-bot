274bf21d2c64a5c20d2b0e29e591a887b079bed8
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-13 23:17:45 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import os
-from pyrogram import Client, filters
+from pyrogram import Client
 
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH") 
@@ -7,13 +7,11 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 app = Client("tia_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-@app.on_message(filters.command("start") & filters.private)
-async def start_cmd(client, message):
-    await message.reply("✅ البوت اشتغل اخيرا")
+import modules.start
+import modules.admin
+import modules.service
+import modules.utils
+import modules.locks
 
-@app.on_message(filters.command("الاوامر"))
-async def help_cmd(client, message):
-    await message.reply("الاوامر: /ban /mute /id")
-
-print("Tia Bot Started ✅")
+print("Tia Bot Started ✅ All modules loaded")
 app.run()
