00e57dc1daf767dfda1aecacb5182a1abae0de04
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-13 21:59:00 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,2 +1,23 @@
-cursor.execute("CREATE TABLE IF NOT EXISTS locks (chat_id INTEGER, lock_type TEXT, PRIMARY KEY(chat_id, lock_type))")
-cursor.execute("CREATE TABLE IF NOT EXISTS settings (key TEXT PRIMARY KEY, value TEXT)")
+import sqlite3
+
+conn = None
+cursor = None
+
+def init_db():
+    global conn, cursor
+    conn = sqlite3.connect('tia.db', check_same_thread=False)
+    cursor = conn.cursor()
+    
+    # جدول القفل
+    cursor.execute("""CREATE TABLE IF NOT EXISTS locks 
+                      (chat_id INTEGER, lock_type TEXT, PRIMARY KEY(chat_id, lock_type))""")
+    
+    # جدول الاعدادات
+    cursor.execute("""CREATE TABLE IF NOT EXISTS settings 
+                      (chat_id INTEGER PRIMARY KEY, welcome TEXT)""")
+    
+    conn.commit()
+    print("Database Connected ✅")
+
+def get_cursor():
+    return cursor, conn
