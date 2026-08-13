1c16e5f81a58d8d733a84c2ada198fcf0c731370
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-13 23:00:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,29 @@
-from pyrogram import Client
-from config import API_ID, API_HASH, BOT_TOKEN
-from database import init_db
+ import os
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
-app = Client("tia_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH") 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if __name__ == "__main__":
-    # 1. يشغل قاعدة البيانات
-    init_db()
-    
-    # 2. هنا الاستدعاء لكل الملفات
-    from modules import start
-    from modules import admin  
-    from modules import locks
-    from modules import settings
-    from modules import fun
-    from modules import service
-    
-    print("="*30)
-    print("Tia Bot Started ✅")
-    print("جميع الموديولات تم تحميلها")
-    print("="*30)
-    
-    # 3. يشغل البوت
-    app.run()
+app = Client(
+    "tia_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
+
+@app.on_message(filters.command("start") & filters.private)
+async def start_cmd(client, message):
+    await message.reply("✅ البوت شغال الحمدلله\nجرب /الاوامر")
+
+@app.on_message(filters.command("الاوامر"))
+async def help_cmd(client, message):
+    await message.reply("**الاوامر:**\n/ban - حظر\n/mute - كتم\n/id - معرفك")
+
+print("==============================")
+print("Database Connected ✅")
+print("Tia Bot Started ✅")
+print("==============================")
+
+app.run()
