63970fca243ec2bf3a122d6af196f105f8d07a81
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-14 20:25:39 +0300

    Refactor lock and unlock commands with filters

--- modules/locks.py
+++ modules/locks.py
@@ -1,21 +1,20 @@
-from pyrogram import Client
-from pyrogram.types import Message
-from pyrogram.enums import ChatPermissions
+from pyrogram import Client, filters
+from pyrogram.types import Message, ChatPermissions # التعديل 1
 
 app = Client.get_client("ProtectionBot")
 
-@app.on_message(commands=["قفل"])
+@app.on_message(filters.command("قفل")) # التعديل 2
 async def lock_cmd(client, message: Message):
-    await message.reply("تم قفل الدردشة 🔒")
     await app.set_chat_permissions(
         message.chat.id,
         ChatPermissions(can_send_messages=False)
     )
+    await message.reply("تم قفل الدردشة 🔒")
 
-@app.on_message(commands=["فتح"])
+@app.on_message(filters.command("فتح")) # التعديل 2
 async def unlock_cmd(client, message: Message):
-    await message.reply("تم فتح الدردشة 🔓")
     await app.set_chat_permissions(
         message.chat.id,
         ChatPermissions(can_send_messages=True)
     )
+    await message.reply("تم فتح الدردشة 🔓")
