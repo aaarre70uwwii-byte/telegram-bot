5e40f831ab5fd951f420a0c4d69c2ff163cee928
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-15 18:42:35 +0300

    Add configuration for bot environment variables

---
+++ config.py
@@ -0,0 +1,8 @@
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "1234567890:ABCdefGhIJKlmNoPQRsTUVwxyZ")
+    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100123456789"))
