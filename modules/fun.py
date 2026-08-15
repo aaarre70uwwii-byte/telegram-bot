8df34721755584c740fe56a410e6ca140d1bdfdd
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-15 18:01:35 +0300

    Delete modules/fun.py

--- modules/fun.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import Client
-from pyrogram.types import Message
-
-app = Client.get_client("ProtectionBot")
-
-@app.on_message(commands=["ping"])
-async def ping_cmd(client, message: Message):
-    await message.reply("pong 🏓")
