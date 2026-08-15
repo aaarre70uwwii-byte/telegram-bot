16b61bcdd06760a461ab2eb5dd3cfa8ebb1363d5
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-15 21:04:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-    import os
-    from dotenv import load_dotenv
+import os
+from dotenv import load_dotenv
 
-    load_dotenv()
+load_dotenv()
 
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "1234567890:ABCdefGhIJKlmNoPQRsTUVwxyZ")
-    OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
-    LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100123456789"))
+BOT_TOKEN = os.getenv("BOT_TOKEN", "1234567890:ABCdefGhIJKlmNoPQRsTUVwxyZ")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100123456789"))
