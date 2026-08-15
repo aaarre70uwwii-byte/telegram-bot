5f7033320e90be621e3de2c042a648e6b2207c3c
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-15 22:46:00 +0300

    Add API and bot token configurations
    Added API_ID, API_HASH, and BOT_TOKEN configuration variables.

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+# للحساب المساعد (يمكنك جلبها من my.telegram.org)
+API_ID = int(os.getenv("API_ID", "123456"))
+API_HASH = os.getenv("API_HASH", "abcdef1234567890abcdef1234567890")
+
+# توكن البوت الأساسي وآيدي المطور
 BOT_TOKEN = os.getenv("BOT_TOKEN", "1234567890:ABCdefGhIJKlmNoPQRsTUVwxyZ")
 OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-100123456789"))
