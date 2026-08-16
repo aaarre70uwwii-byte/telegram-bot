b2bee5da991bc3e080f385ef10546083f6c38e17
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-16 19:51:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
-MAX_SONG_DURATION = int(os.getenv("MAX_SONG_DURATION", 60))
+MAX_SONG_DURATION = int(os.getenv("MAX_SONG_DURATION", "60"))
+MESSAGE_DUPLICATE_LIMIT = int(os.getenv("MESSAGE_DUPLICATE_LIMIT", "2")) # ضفته عشانك
+NIXPACKS_PACKAGES = os.getenv("NIXPACKS_PACKAGES", "ffmpeg")
