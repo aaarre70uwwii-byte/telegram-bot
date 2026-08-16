47a41ae2e38ba514e3dc1f5c741edc724588f1e8
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-16 19:59:34 +0300

    Delete plugins/start.py

--- plugins/start.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-import config
-
-@Client.on_message(filters.command("start") & filters.private)
-async def start_command(client, message):
-    await message.reply_text(
-        f"مرحباً بك {message.from_user.mention} في بوت الحماية والأغاني!\n"
-        "أرسل /help لرؤية الأوامر."
-    )
