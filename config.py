b45e1a140eafebb49834886929a9a44a2880fa98
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-16 19:58:24 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,15 @@
 import os
 
+# === الحماية ===
 API_ID = int(os.getenv("API_ID"))
 API_HASH = os.getenv("API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
+ALLOWED_USERS = [OWNER_ID, ADMIN_ID] # بس انت والادمن
+
+# === اعدادات البوت ===
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
 MAX_SONG_DURATION = int(os.getenv("MAX_SONG_DURATION", "60"))
-MESSAGE_DUPLICATE_LIMIT = int(os.getenv("MESSAGE_DUPLICATE_LIMIT", "2")) # ضفته عشانك
+MESSAGE_DUPLICATE_LIMIT = int(os.getenv("MESSAGE_DUPLICATE_LIMIT", "2"))
 NIXPACKS_PACKAGES = os.getenv("NIXPACKS_PACKAGES", "ffmpeg")
