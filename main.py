1355fc6096f44018ccbe22245a5aadb5c011437a
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-17 22:53:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-import os
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
 import asyncio
-from pyrogram import Client, filters
-from pyrogram.types import Message
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+# استدعاء كل الملفات
+from commands import *
+from buttons import *
 
-app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client(
+    "tia_bot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
+)
 
-@app.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply("اهلا 👋 البوت شغال")
+async def main():
+    print("『𝐓𝐢𝐚』 البوت شغال الان ...")
+    await app.start()
+    print(f"『𝐓𝐢𝐚』 تم تسجيل الدخول: {(await app.get_me()).first_name}")
+    await asyncio.Event().wait()
 
-@app.on_message(filters.command("help"))
-async def help_cmd(client, message):
-    await message.reply("/start - تشغيل البوت")
-
-app.run()
+if __name__ == "__main__":
+    asyncio.run(main())
