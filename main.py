1b9ba82ff4a408cb41ccbd5efdc35e2cd7f50ddd
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-23 20:21:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,27 @@
 import telebot
 import os
 import sys
+import importlib
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
+from config import * # يقرأ التوكن والايدي من هنا
 from utils.keyboards import *
-from cogs import menu, locks
 
-# ========== قراءة المتغيرات ==========
-TOKEN = os.getenv("TOKEN") 
-if not TOKEN:
-    raise ValueError("خطأ: التوكن TOKEN مش موجود في المتغيرات")
-
-المطور_الاساسي = int(os.getenv("DEVELOPER_ID", "7488375443"))
-admins = [المطور_الاساسي]
-
-# ========== تشغيل البوت ==========
 bot = telebot.TeleBot(TOKEN, parse_mode="Markdown")
 
-# استدعاء كل الملفات وتمرير المتغيرات
-menu.setup_menu(bot, المطور_الاساسي, admins)
-locks.setup_locks(bot)
+# ========== استدعاء كل ملفات cogs تلقائي ==========
+cogs_list = ["menu", "locks"] # ضيف اسم اي ملف جديد هنا
 
+for cog in cogs_list:
+    module = importlib.import_module(f"cogs.{cog}")
+    if hasattr(module, "setup"):
+        module.setup(bot, المطور_الاساسي, admins)
+    print(f"✅ تم تحميل: {cog}.py")
+
+# ========== تشغيل البوت ==========
 print("="*50)
-print(f"✅ بوت 𝐓𝐢𝐚 شغال")
+print(f"✅ بوت {اسم_البوت} شغال الان")
 print(f"✅ المطور: {المطور_الاساسي}")
 print("="*50)
 
