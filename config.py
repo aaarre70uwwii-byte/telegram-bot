222805ab908cdd162dbce879f5ca19630ba3f3f9
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-25 00:47:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-المطور_الاساسي = 7488375443
-اسم_البوت = "𝐓𝐢𝐚"
-admins = [7488375443]
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+المطور_الاساسي = int(os.getenv("DEVELOPER_ID", "7488375443")) # حط ايديك هنا
+اسم_البوت = os.getenv("BOT_NAME", "𝐓𝐢𝐚")
+admins = [] # بنضيف مشرفين لاحقا
+
+# متغيرات الحماية
+MAINTENANCE = os.getenv("MAINTENANCE", "False") == "True"
