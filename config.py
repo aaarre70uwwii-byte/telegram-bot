0544d3abfdba8217e2480d28f0481aa7886af86e
aaarre70uwwii-byte <aaarre70uwwii@gmail.com>
2026-08-25 21:11:34 +0300

    Fix config to read from environment variables
    Updated config.py to load TOKEN, DEV_ID, ADMINS from Railway environment variables instead of hardcoding.
    Fixes AttributeError: module 'config' has no attribute 'TOKEN'

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-
-المطور_الاساسي = 7488375443 # حط ايديك هنا
-admins = [7488375443] # ضيف ايديات الادمنية هنا
-
-اسم_البوت = "𝐓𝐢𝐚"
-MAINTENANCE = False
+TOKEN = os.getenv("BOT_TOKEN")
+DEV = int(os.getenv("DEV_ID"))
+ADMINS = os.getenv("ADMINS", "")
+admins = [int(x) for x in ADMINS.split(",") if x]
